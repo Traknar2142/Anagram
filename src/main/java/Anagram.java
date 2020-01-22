1448e407f20a12ef679fd225182fa0547bdaf5ea
Prokhorenko Viktor Vadimovich <viktor4516@yandex.ru>
2020-01-22 21:06:57 +0200

    Task 1 Anagrams

---
+++ src/main/java/Anagram.java
@@ -0,0 +1,27 @@
+public class Anagram {
+    public static String createAnagram(String inpitString){
+        String outputString = "";
+        String[] massOfWords = inpitString.split("\\s");
+
+        for (int i = 0; i <= massOfWords.length-1; i++){
+            char[] massOfChar = massOfWords[i].toCharArray();
+            char[]  temp= new char[massOfChar.length];
+            int count=0;
+            for (int j = massOfChar.length-1; j >= 0; j--){
+                if (Character.isLetter(massOfChar[j])){
+                    temp[count] = massOfChar[j];
+                    count++;
+                }
+            }
+            count = 0;
+            for (int j = 0; j <= massOfChar.length-1; j++){
+                if (Character.isLetter(massOfChar[j])){
+                    massOfChar[j] = temp[count];
+                    count++;
+                }
+            }
+            outputString = outputString + new String(massOfChar) + " ";
+        }
+        return outputString;
+    }
+}
