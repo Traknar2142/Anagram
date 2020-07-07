0439475baabaa3bc31fd9c26ba289d4b32a164de
Traknar <Traknar@DESKTOP-3M51OS8>
2020-07-07 15:16:18 +0300

    Added JUnit tests Added check exceptions

--- src/main/java/com/traknar/Anagram.java
+++ src/main/java/com/traknar/Anagram.java
@@ -1,28 +1,34 @@
 package com.traknar;
-public class Anagram {
-    public static String createAnagram(String inpitString){
-        String outputString = "";
-        String[] massOfWords = inpitString.split("\\s");
 
-        for (int i = 0; i <= massOfWords.length-1; i++){
-            char[] massOfChar = massOfWords[i].toCharArray();
-            char[]  temp= new char[massOfChar.length];
-            int count=0;
-            for (int j = massOfChar.length-1; j >= 0; j--){
-                if (Character.isLetter(massOfChar[j])){
-                    temp[count] = massOfChar[j];
-                    count++;
-                }
-            }
-            count = 0;
-            for (int j = 0; j <= massOfChar.length-1; j++){
-                if (Character.isLetter(massOfChar[j])){
-                    massOfChar[j] = temp[count];
-                    count++;
-                }
-            }
-            outputString = outputString + new String(massOfChar) + " ";
-        }
-        return outputString;
-    }
+public class Anagram {
+	private static String removeLastChar(String s) {
+		return (s == null || s.length() == 0) ? null : (s.substring(0, s.length() - 1));
+	}
+
+	public static String createAnagram(String inpitString) {
+		String outputString = "";
+		String[] massOfWords = inpitString.split("\\s");
+
+		for (int i = 0; i <= massOfWords.length - 1; i++) {
+			char[] massOfChar = massOfWords[i].toCharArray();
+			char[] temp = new char[massOfChar.length];
+			int count = 0;
+			for (int j = massOfChar.length - 1; j >= 0; j--) {
+				if (Character.isLetter(massOfChar[j])) {
+					temp[count] = massOfChar[j];
+					count++;
+				}
+			}
+			count = 0;
+			for (int j = 0; j <= massOfChar.length - 1; j++) {
+				if (Character.isLetter(massOfChar[j])) {
+					massOfChar[j] = temp[count];
+					count++;
+				}
+			}
+			outputString = outputString + new String(massOfChar) + " ";
+		}
+
+		return removeLastChar(outputString);
+	}
 }
--- src/main/java/com/traknar/Main.java
+++ src/main/java/com/traknar/Main.java
@@ -1,12 +1,19 @@
 package com.traknar;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader((System.in)));
-        String inputString = reader.readLine();
-        System.out.println(Anagram.createAnagram(inputString));
-    }
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader((System.in)));
+		try {
+			String inputString = reader.readLine();
+			String result = Anagram.createAnagram(inputString);
+			System.out.println(result);
+		} catch (IOException | NullPointerException e) {
+			System.out.println("Enter words");
+		}
+
+	}
 }
